db828cf535538d2f2b8b5a8f72d92bc255eb9cfa
lbugnion <lbugnion@localhost>
2009-11-30 20:19:42 +0000

    V3/alpha3 for VS10

---
+++ VS10/ProjectForTemplate/ProjectForTemplate/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight.Threading;
+
+namespace ProjectForTemplate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            DispatcherExtensions.Initialize();
+        }
+    }
+}
