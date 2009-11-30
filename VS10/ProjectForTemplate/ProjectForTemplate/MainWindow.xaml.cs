250c7d8de4528cb7be9cc5cd2abfa6003698038a
Laurent Bugnion <laurent@galasoft.ch>
2009-11-30 20:19:42 +0000

    V3/alpha3 for VS10

---
+++ VS10/ProjectForTemplate/ProjectForTemplate/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using ProjectForTemplate.ViewModel;
+
+namespace ProjectForTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
