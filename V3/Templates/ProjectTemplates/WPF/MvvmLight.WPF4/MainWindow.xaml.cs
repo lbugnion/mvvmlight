62a039d3c70030d4d82b649821caaa1ddcc2d589
lbugnion <lbugnion@localhost>
2012-01-02 18:35:35 +0100

    V3: Added specific solution for SL5 and WP71. V3: Updated Nuget with SL5. V3: Various improvements to templates.

--- V3/Templates/ProjectTemplates/WPF/MvvmLight.WPF4/MainWindow.xaml.cs
+++ V3/Templates/ProjectTemplates/WPF/MvvmLight.WPF4/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ namespace $safeprojectname$
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
