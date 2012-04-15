2c160a32d7c30254f929e127ca9f4a838f14f91f
Laurent <Laurent@Precision-Win8b.home>
2012-04-15 22:54:12 +0200

    Improved messenger constraints: checking is a message implements an interface or inherits a base class.

---
+++ GalaSoft.MvvmLight/GalaSoft.MvvmLight.Test (NET35)/Messaging/ITestMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GalaSoft.MvvmLight.Test.Messaging
+{
+    public interface ITestMessage
+    {
+        string Content
+        {
+            get;
+        }
+    }
+}
---
+++ GalaSoft.MvvmLight/GalaSoft.MvvmLight.Test (NET35)/Messaging/TestMessageBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace GalaSoft.MvvmLight.Test.Messaging
+{
+    public class TestMessageBase : MessageBase, ITestMessage
+    {
+        public TestMessageBase(object sender)
+            : base(sender)
+        { 
+        }
+
+        public string Content
+        {
+            get;
+            set;
+        }
+    }
+}
