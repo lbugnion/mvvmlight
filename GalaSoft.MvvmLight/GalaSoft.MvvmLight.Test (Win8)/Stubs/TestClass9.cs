482c21170956423cee288930d7fd5b3dd8948490
Laurent Bugnion <laurent@galasoft.ch>
2012-12-17 22:23:48 +0100

    Deleted Win8 separate folder and put Win8 files together with the others.

---
+++ GalaSoft.MvvmLight/GalaSoft.MvvmLight.Test (Win8)/Stubs/TestClass9.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaSoft.MvvmLight.Test.Stubs
+{
+    public class TestClass9
+    {
+        public TestClass9()
+        {
+            
+        }
+
+        internal TestClass9(string param)
+        {
+            
+        }
+    }
+}
