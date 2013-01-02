d91d0a6c68380731cb69ee9ee1ff263f0e624002
Laurent Bugnion <laurent@galasoft.ch>
2013-01-02 12:15:35 +0100

    Various changes to templates - Fixing .NET 4.5 references - Restoring default logos and splash screen for Windows 8 (to allow detection by WACK tool) - Simplified folder structure - Reduced path length due to some issues on some OSes - Using LayoutAwarePage for Windows 8

--- Templates/CSharp/ProjectTemplates/Win8/MvvmLight.Win8/MainPage.xaml.cs
+++ Templates/CSharp/ProjectTemplates/Win8/MvvmLight.Win8/MainPage.xaml.cs
@@ -14,19 +14,33 @@ using Windows.UI.Xaml.Navigation;
 
 namespace $safeprojectname$
 {
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
         /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.  The Parameter
-        /// property is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
         {
         }
     }
