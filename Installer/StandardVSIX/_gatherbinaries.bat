2201938ec02b39a9fa303e71223dcd3f4584c5ab
Laurent Bugnion <laurent@galasoft.ch>
2014-05-08 11:55:09 +0200

    Working on Installers (VSIX)

--- Installer/StandardVSIX/_gatherbinaries.bat
+++ Installer/StandardVSIX/_gatherbinaries.bat
@@ -3,15 +3,12 @@ rem @echo off
 if not exist ..\InstallItems\VSIX md ..\InstallItems\VSIX
 if not exist ..\InstallItems\VSIX\Release md ..\InstallItems\VSIX\Release
 
-if exist ".\MvvmLight.VS10\bin\Release\MvvmLight.VS2010.vsix" copy ".\MvvmLight.VS10\bin\Release\MvvmLight.VS2010.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2010.vsix
-if exist ".\MvvmLight.VS11\bin\Release\MvvmLight.VS2012.vsix" copy ".\MvvmLight.VS11\bin\Release\MvvmLight.VS2012.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2012.vsix
+if exist ".\MvvmLight.VS10\bin\Debug\MvvmLight.VS2010.vsix" copy ".\MvvmLight.VS10\bin\Debug\MvvmLight.VS2010.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2010.vsix
+if exist ".\MvvmLight.VS11\bin\Debug\MvvmLight.VS2012.vsix" copy ".\MvvmLight.VS11\bin\Debug\MvvmLight.VS2012.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2012.vsix
+if exist ".\MvvmLight.VS12\bin\Debug\MvvmLight.VS2013.vsix" copy ".\MvvmLight.VS12\bin\Debug\MvvmLight.VS2013.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013.vsix
 
-if exist ".\MvvmLight.VCSx10\bin\Release\MvvmLight.WPFExpress2010.vsix" copy ".\MvvmLight.VCSx10\bin\Release\MvvmLight.WPFExpress2010.vsix" ..\InstallItems\VSIX\Release\MvvmLight.WPFExpress2010.vsix
-if exist ".\MvvmLight.VCSx11\bin\Release\MvvmLight.WPFExpress2012.vsix" copy ".\MvvmLight.VCSx11\bin\Release\MvvmLight.WPFExpress2012.vsix" ..\InstallItems\VSIX\Release\MvvmLight.WPFExpress2012.vsix
-
-if exist ".\MvvmLight.VSWin8x11\bin\Release\MvvmLight.Win8Express2012.vsix" copy ".\MvvmLight.VSWin8x11\bin\Release\MvvmLight.Win8Express2012.vsix" ..\InstallItems\VSIX\Release\MvvmLight.Win8Express2012.vsix
-if exist ".\MvvmLight.VSWP8x11\bin\Release\MvvmLight.WP8Express2012.vsix" copy ".\MvvmLight.VSWP8x11\bin\Release\MvvmLight.WP8Express2012.vsix" ..\InstallItems\VSIX\Release\MvvmLight.WP8Express2012.vsix
-
-if exist ".\MvvmLight.VWDx10\bin\Release\MvvmLight.SilverlightExpress2010.vsix" copy ".\MvvmLight.VWDx10\bin\Release\MvvmLight.SilverlightExpress2010.vsix" ..\InstallItems\VSIX\Release\MvvmLight.SilverlightExpress2010.vsix
-if exist ".\MvvmLight.VWDx11\bin\Release\MvvmLight.SilverlightExpress2012.vsix" copy ".\MvvmLight.VWDx11\bin\Release\MvvmLight.SilverlightExpress2012.vsix" ..\InstallItems\VSIX\Release\MvvmLight.SilverlightExpress2012.vsix
+if exist ".\MvvmLight.VS12SlExpress\bin\Debug\MvvmLight.VS2013SilverlightExpress.vsix"  copy ".\MvvmLight.VS12SlExpress\bin\Debug\MvvmLight.VS2013SilverlightExpress.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013SilverlightExpress.vsix
+if exist ".\MvvmLight.VS12WinExpress\bin\Debug\MvvmLight.VS2013WindowsExpress.vsix" copy ".\MvvmLight.VS12WinExpress\bin\Debug\MvvmLight.VS2013WindowsExpress.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013WindowsExpress.vsix
+if exist ".\MvvmLight.VS12WpExpress\bin\Debug\MvvmLight.VS2013WinPhoneExpress.vsix"  copy ".\MvvmLight.VS12WpExpress\bin\Debug\MvvmLight.VS2013WinPhoneExpress.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013WinPhoneExpress.vsix
+if exist ".\MvvmLight.VS12WpfExpress\bin\Debug\MvvmLight.VS2013WpfExpress.vsix" copy ".\MvvmLight.VS12WpfExpress\bin\Debug\MvvmLight.VS2013WpfExpress.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013WpfExpress.vsix
 
