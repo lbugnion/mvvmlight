e60c09b6187df35902f5f429bc849c6c58ec0501
Laurent Bugnion <laurent@galasoft.ch>
2015-02-13 20:35:15 +0100

    Updated Flowers sample to Xamarin.Forms V1.3

---
+++ Samples/Flowers/Flowers.Forms/Flowers.Forms/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flowers.Data.ViewModel;
+using Flowers.Forms.Helpers;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using Xamarin.Forms;
+
+namespace Flowers.Forms
+{
+    public partial class App : Application
+    {
+        public const string ShowCommentsPageKey = "ShowCommentsPage";
+
+        public App()
+        {
+            InitializeComponent();
+
+            // First time initialization
+            var nav = new NavigationService();
+            nav.Configure(ViewModelLocator.DetailsPageKey, typeof(DetailsPage));
+            nav.Configure(ViewModelLocator.AddCommentPageKey, typeof(AddCommentPage));
+            nav.Configure(App.ShowCommentsPageKey, typeof(ShowCommentsPage));
+            SimpleIoc.Default.Register<INavigationService>(() => nav);
+
+            var dialog = new DialogService();
+            SimpleIoc.Default.Register<IDialogService>(() => dialog);
+
+            var navPage = new NavigationPage(new MainPage());
+
+            nav.Initialize(navPage);
+            dialog.Initialize(navPage);
+
+            // The root page of your application
+            MainPage = navPage;
+        }
+    }
+}
