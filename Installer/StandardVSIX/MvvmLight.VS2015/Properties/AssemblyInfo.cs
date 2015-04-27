1ef618d0181bdbd8429e617fd25326ee5b55933c
Laurent Bugnion <laurent@galasoft.ch>
2015-04-27 10:27:53 -0700

    Added VS2015 VSIX

--- Installer/StandardVSIX/MvvmLight.VS2015/Properties/AssemblyInfo.cs
+++ Installer/StandardVSIX/MvvmLight.VS2015/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvvmLight.VS2013")]
+[assembly: AssemblyTitle("MvvmLight.VS2015")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MvvmLight.VS2013")]
+[assembly: AssemblyProduct("MvvmLight.VS2015")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Installer/StandardVSIX/_gatherbinaries.bat
+++ Installer/StandardVSIX/_gatherbinaries.bat
@@ -3,12 +3,7 @@ rem @echo off
 if not exist ..\InstallItems\VSIX md ..\InstallItems\VSIX
 if not exist ..\InstallItems\VSIX\Release md ..\InstallItems\VSIX\Release
 
-if exist ".\MvvmLight.VS10\bin\Release\MvvmLight.VS2010.vsix" copy ".\MvvmLight.VS10\bin\Release\MvvmLight.VS2010.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2010.vsix
-if exist ".\MvvmLight.VS11\bin\Release\MvvmLight.VS2012.vsix" copy ".\MvvmLight.VS11\bin\Release\MvvmLight.VS2012.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2012.vsix
-if exist ".\MvvmLight.VS12\bin\Release\MvvmLight.VS2013.vsix" copy ".\MvvmLight.VS12\bin\Release\MvvmLight.VS2013.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013.vsix
-
-if exist ".\MvvmLight.VS12SlExpress\bin\Release\MvvmLight.VS2013SilverlightExpress.vsix"  copy ".\MvvmLight.VS12SlExpress\bin\Release\MvvmLight.VS2013SilverlightExpress.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013SilverlightExpress.vsix
-if exist ".\MvvmLight.VS12WinExpress\bin\Release\MvvmLight.VS2013WindowsExpress.vsix" copy ".\MvvmLight.VS12WinExpress\bin\Release\MvvmLight.VS2013WindowsExpress.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013WindowsExpress.vsix
-if exist ".\MvvmLight.VS12WpExpress\bin\Release\MvvmLight.VS2013WinPhoneExpress.vsix"  copy ".\MvvmLight.VS12WpExpress\bin\Release\MvvmLight.VS2013WinPhoneExpress.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013WinPhoneExpress.vsix
-if exist ".\MvvmLight.VS12WpfExpress\bin\Release\MvvmLight.VS2013WpfExpress.vsix" copy ".\MvvmLight.VS12WpfExpress\bin\Release\MvvmLight.VS2013WpfExpress.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013WpfExpress.vsix
-
+if exist ".\MvvmLight.VS2010\bin\Release\MvvmLight.VS2010.vsix" copy ".\MvvmLight.VS2010\bin\Release\MvvmLight.VS2010.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2010.vsix
+if exist ".\MvvmLight.VS2012\bin\Release\MvvmLight.VS2012.vsix" copy ".\MvvmLight.VS2012\bin\Release\MvvmLight.VS2012.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2012.vsix
+if exist ".\MvvmLight.VS2013\bin\Release\MvvmLight.VS2013.vsix" copy ".\MvvmLight.VS2013\bin\Release\MvvmLight.VS2013.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2013.vsix
+if exist ".\MvvmLight.VS2015\bin\Release\MvvmLight.VS2015.vsix" copy ".\MvvmLight.VS2015\bin\Release\MvvmLight.VS2015.vsix" ..\InstallItems\VSIX\Release\MvvmLight.VS2015.vsix
