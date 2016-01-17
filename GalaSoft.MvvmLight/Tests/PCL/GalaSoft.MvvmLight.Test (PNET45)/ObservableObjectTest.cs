d3a603de38fc011578ef7d1fe035a7fffe7afc96
Laurent Bugnion <laurent@galasoft.ch>
2016-01-17 21:18:56 +0100

    Issue 7701: RaisePC public ? NOTE: This can be a breaking change if you were overriding RaisePropertyChanged in your code before.
    Issue 7564: The RaisePropertyChanged Overloads Contain Duplicate Code.
    This is solved now in ViewModelBase and ObservableObject. All calls are routed
    through ObservableObject.RaisePropertyChanged(string). If you override this method only in your VMs,
    it will always be called if you call any of the Set methods, RaisePropertyChanged(string)
    or RaisePropertyChanged(Expression).

---
+++ GalaSoft.MvvmLight/Tests/PCL/GalaSoft.MvvmLight.Test (PNET45)/ObservableObjectTest.cs
@@ -0,0 +1,105 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Test.ViewModel;
+
+#if NETFX_CORE || (WINDOWS_PHONE && !WINDOWS_PHONE7)
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+namespace GalaSoft.MvvmLight.Test
+{
+    [TestClass]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class ObservableObjectTest
+    {
+        [TestMethod]
+        public void RaisePropertyChanged_WithString_ShouldCallRaisePropertyChangedWithStringOnly()
+        {
+            var testObject = new TestObservableObject();
+
+            var eventWasRaised = false;
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithString")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Assert.IsFalse(testObject.BoolPropertyWithString);
+            testObject.BoolPropertyWithString = true;
+            Assert.IsTrue(testObject.BoolPropertyWithString);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsFalse(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+
+        [TestMethod]
+        public void RaisePropertyChanged_WithExpression_ShouldCallRaisePropertyChangedWithStringAndWithExpression()
+        {
+            var testObject = new TestObservableObject();
+
+            var eventWasRaised = false;
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithExpression")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Assert.IsFalse(testObject.BoolPropertyWithExpression);
+            testObject.BoolPropertyWithExpression = true;
+            Assert.IsTrue(testObject.BoolPropertyWithExpression);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+
+        [TestMethod]
+        public void RaisePropertyChanged_WithSetAndExpression_ShouldCallRaisePropertyChangedWithStringAndWithExpression()
+        {
+            var testObject = new TestObservableObject();
+
+            var eventWasRaised = false;
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithSetAndExpression")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Assert.IsFalse(testObject.BoolPropertyWithSetAndExpression);
+            testObject.BoolPropertyWithSetAndExpression = true;
+            Assert.IsTrue(testObject.BoolPropertyWithSetAndExpression);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+
+        [TestMethod]
+        public void RaisePropertyChanged_WithSetAndString_ShouldCallRaisePropertyChangedWithStringOnly()
+        {
+            var testObject = new TestObservableObject();
+
+            var eventWasRaised = false;
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithSetAndString")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Assert.IsFalse(testObject.BoolPropertyWithSetAndString);
+            testObject.BoolPropertyWithSetAndString = true;
+            Assert.IsTrue(testObject.BoolPropertyWithSetAndString);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsFalse(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+    }
+}
---
+++ GalaSoft.MvvmLight/Tests/PCL/GalaSoft.MvvmLight.Test (PNET45)/ViewModelBaseAndPropertyChangedTest.cs
@@ -0,0 +1,249 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Test.ViewModel;
+
+#if NETFX_CORE || (WINDOWS_PHONE && !WINDOWS_PHONE7)
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+namespace GalaSoft.MvvmLight.Test
+{
+    [TestClass]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class ViewModelBaseAndPropertyChangedTest
+    {
+        [TestMethod]
+        public void RaisePropertyChanged_WithString_ShouldCallRaisePropertyChangedWithStringOnly()
+        {
+            var testObject = new TestViewModelForPropertyChanged();
+
+            var eventWasRaised = false;
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithString")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Assert.IsFalse(testObject.BoolPropertyWithString);
+            testObject.BoolPropertyWithString = true;
+            Assert.IsTrue(testObject.BoolPropertyWithString);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsFalse(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+
+        [TestMethod]
+        public void RaisePropertyChanged_WithExpression_ShouldCallRaisePropertyChangedWithStringAndWithExpression()
+        {
+            var testObject = new TestViewModelForPropertyChanged();
+
+            var eventWasRaised = false;
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithExpression")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Assert.IsFalse(testObject.BoolPropertyWithExpression);
+            testObject.BoolPropertyWithExpression = true;
+            Assert.IsTrue(testObject.BoolPropertyWithExpression);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+
+        [TestMethod]
+        public void RaisePropertyChanged_WithExpressionAndMessage_ShouldCallRaisePropertyChangedWithStringAndWithExpression()
+        {
+            var testObject = new TestViewModelForPropertyChanged();
+
+            var eventWasRaised = false;
+            var messageWasReceived = false;
+
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithExpressionAndMessage")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Messenger.Default.Register<PropertyChangedMessage<bool>>(
+                this,
+                message =>
+                {
+                    if (message.PropertyName == "BoolPropertyWithExpressionAndMessage"
+                        && message.Sender == testObject)
+                    {
+                        messageWasReceived = true;
+                    }
+                });
+
+            Assert.IsFalse(testObject.BoolPropertyWithExpressionAndMessage);
+            testObject.BoolPropertyWithExpressionAndMessage = true;
+            Assert.IsTrue(testObject.BoolPropertyWithExpressionAndMessage);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(messageWasReceived);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+
+        [TestMethod]
+        public void RaisePropertyChanged_WithSetAndExpression_ShouldCallRaisePropertyChangedWithStringAndWithExpression()
+        {
+            var testObject = new TestViewModelForPropertyChanged();
+
+            var eventWasRaised = false;
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithSetAndExpression")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Assert.IsFalse(testObject.BoolPropertyWithSetAndExpression);
+            testObject.BoolPropertyWithSetAndExpression = true;
+            Assert.IsTrue(testObject.BoolPropertyWithSetAndExpression);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+
+        [TestMethod]
+        public void RaisePropertyChanged_WithSetAndExpressionAndMessage_ShouldCallRaisePropertyChangedWithStringAndWithExpression()
+        {
+            var testObject = new TestViewModelForPropertyChanged();
+
+            var eventWasRaised = false;
+            var messageWasReceived = false;
+
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithSetAndExpressionAndMessage")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Messenger.Default.Register<PropertyChangedMessage<bool>>(
+                this,
+                message =>
+                {
+                    if (message.PropertyName == "BoolPropertyWithSetAndExpressionAndMessage"
+                        && message.Sender == testObject)
+                    {
+                        messageWasReceived = true;
+                    }
+                });
+
+            Assert.IsFalse(testObject.BoolPropertyWithSetAndExpressionAndMessage);
+            testObject.BoolPropertyWithSetAndExpressionAndMessage = true;
+            Assert.IsTrue(testObject.BoolPropertyWithSetAndExpressionAndMessage);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(messageWasReceived);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+
+        [TestMethod]
+        public void RaisePropertyChanged_WithSetAndString_ShouldCallRaisePropertyChangedWithStringOnly()
+        {
+            var testObject = new TestViewModelForPropertyChanged();
+
+            var eventWasRaised = false;
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithSetAndString")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Assert.IsFalse(testObject.BoolPropertyWithSetAndString);
+            testObject.BoolPropertyWithSetAndString = true;
+            Assert.IsTrue(testObject.BoolPropertyWithSetAndString);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsFalse(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+
+        [TestMethod]
+        public void RaisePropertyChanged_WithSetAndStringAndMessage_ShouldCallRaisePropertyChangedWithStringOnly()
+        {
+            var testObject = new TestViewModelForPropertyChanged();
+
+            var eventWasRaised = false;
+            var messageWasReceived = false;
+
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithSetAndStringAndMessage")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Messenger.Default.Register<PropertyChangedMessage<bool>>(
+                this,
+                message =>
+                {
+                    if (message.PropertyName == "BoolPropertyWithSetAndStringAndMessage"
+                        && message.Sender == testObject)
+                    {
+                        messageWasReceived = true;
+                    }
+                });
+
+            Assert.IsFalse(testObject.BoolPropertyWithSetAndStringAndMessage);
+            testObject.BoolPropertyWithSetAndStringAndMessage = true;
+            Assert.IsTrue(testObject.BoolPropertyWithSetAndStringAndMessage);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(messageWasReceived);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsFalse(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+
+        [TestMethod]
+        public void RaisePropertyChanged_WithStringAndMessage_ShouldCallRaisePropertyChangedWithStringOnly()
+        {
+            var testObject = new TestViewModelForPropertyChanged();
+
+            var eventWasRaised = false;
+            var messageWasReceived = false;
+
+            testObject.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "BoolPropertyWithStringAndMessage")
+                {
+                    eventWasRaised = true;
+                }
+            };
+
+            Messenger.Default.Register<PropertyChangedMessage<bool>>(
+                this,
+                message =>
+                {
+                    if (message.PropertyName == "BoolPropertyWithStringAndMessage"
+                        && message.Sender == testObject)
+                    {
+                        messageWasReceived = true;
+                    }
+                });
+
+            Assert.IsFalse(testObject.BoolPropertyWithStringAndMessage);
+            testObject.BoolPropertyWithStringAndMessage = true;
+            Assert.IsTrue(testObject.BoolPropertyWithStringAndMessage);
+            Assert.IsTrue(eventWasRaised);
+            Assert.IsTrue(messageWasReceived);
+            Assert.IsTrue(testObject.RaisePropertyChangedWithPropertyNameWasCalled);
+            Assert.IsFalse(testObject.RaisePropertyChangedWithExpressionWasCalled);
+        }
+    }
+}
