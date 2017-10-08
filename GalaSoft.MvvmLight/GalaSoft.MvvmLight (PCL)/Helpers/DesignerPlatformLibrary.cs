b1a6b1df2960730f100f75e522e139d512814ae1
Lbugnion <lbugnion@outlook.com>
2017-10-08 16:26:43 +0200

    Refactoring design mode detection.

--- GalaSoft.MvvmLight/GalaSoft.MvvmLight (PCL)/Helpers/DesignerPlatformLibrary.cs
+++ GalaSoft.MvvmLight/GalaSoft.MvvmLight (PCL)/Helpers/DesignerPlatformLibrary.cs
@@ -14,6 +14,8 @@
 // ****************************************************************************
 
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace GalaSoft.MvvmLight.Helpers
 {
@@ -67,6 +69,194 @@ namespace GalaSoft.MvvmLight.Helpers
 
 
         private static DesignerPlatformLibrary? _detectedDesignerPlatformLibrary;
+
+        private static bool? _isInDesignMode;
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                if (!_isInDesignMode.HasValue)
+                {
+#if PORTABLE
+                    _isInDesignMode = IsInDesignModePortable();
+#elif SILVERLIGHT
+                    _isInDesignMode = System.ComponentModel.DesignerProperties.IsInDesignTool;
+#elif NETFX_CORE
+                    _isInDesignMode = DesignMode.DesignModeEnabled;
+#elif XAMARIN
+                    _isInDesignMode = false;
+#else
+                    var prop = System.ComponentModel.DesignerProperties.IsInDesignModeProperty;
+                    _isInDesignMode
+                        = (bool)System.ComponentModel.DependencyPropertyDescriptor
+                                        .FromProperty(prop, typeof(System.Windows.FrameworkElement))
+                                        .Metadata.DefaultValue;
+#endif
+                }
+
+                return _isInDesignMode.Value;
+            }
+        }
+
+#if PORTABLE
+        private static bool IsInDesignModePortable()
+        {
+            // As a portable lib, we need see what framework we're runnign on
+            // and use reflection to get the designer value
+
+            var platform = DesignerLibrary.DetectedDesignerLibrary;
+
+            if (platform == DesignerPlatformLibrary.WinRt)
+            {
+                return IsInDesignModeMetro();
+            }
+
+            if (platform == DesignerPlatformLibrary.Silverlight)
+            {
+                var desMode = IsInDesignModeSilverlight();
+                if (!desMode)
+                {
+                    desMode = IsInDesignModeNet(); // hard to tell these apart in the designer
+                }
+
+                return desMode;
+            }
+
+            if (platform == DesignerPlatformLibrary.Net)
+            {
+                return IsInDesignModeNet();
+            }
+
+            return false;
+        }
+
+        private static bool IsInDesignModeSilverlight()
+        {
+            try
+            {
+                var dm = Type.GetType("System.ComponentModel.DesignerProperties, System.Windows, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e");
+
+                if (dm == null)
+                {
+                    return false;
+                }
+
+                var dme = dm.GetTypeInfo().GetDeclaredProperty("IsInDesignTool");
+
+                if (dme == null)
+                {
+                    return false;
+                }
+
+                return (bool)dme.GetValue(null, null);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static bool IsInDesignModeMetro()
+        {
+            try
+            {
+                var dm = Type.GetType("Windows.ApplicationModel.DesignMode, Windows, ContentType=WindowsRuntime");
+
+                var dme = dm.GetTypeInfo().GetDeclaredProperty("DesignModeEnabled");
+                return (bool)dme.GetValue(null, null);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static bool IsInDesignModeNet()
+        {
+            try
+            {
+                var dm =
+                    Type.GetType(
+                        "System.ComponentModel.DesignerProperties, PresentationFramework, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+
+                if (dm == null)
+                {
+                    return false;
+                }
+
+                var dmp = dm.GetTypeInfo().GetDeclaredField("IsInDesignModeProperty").GetValue(null);
+
+                var dpd =
+                    Type.GetType(
+                        "System.ComponentModel.DependencyPropertyDescriptor, WindowsBase, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+                var typeFe =
+                    Type.GetType("System.Windows.FrameworkElement, PresentationFramework, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+
+                if (dpd == null
+                    || typeFe == null)
+                {
+                    return false;
+                }
+
+                var fromPropertys = dpd
+                    .GetTypeInfo()
+                    .GetDeclaredMethods("FromProperty")
+                    .ToList();
+
+                if (fromPropertys == null
+                    || fromPropertys.Count == 0)
+                {
+                    return false;
+                }
+
+                var fromProperty = fromPropertys
+                    .FirstOrDefault(mi => mi.IsPublic && mi.IsStatic && mi.GetParameters().Length == 2);
+
+                if (fromProperty == null)
+                {
+                    return false;
+                }
+
+                var descriptor = fromProperty.Invoke(null, new[] { dmp, typeFe });
+
+                if (descriptor == null)
+                {
+                    return false;
+                }
+
+                var metaProp = dpd.GetTypeInfo().GetDeclaredProperty("Metadata");
+
+                if (metaProp == null)
+                {
+                    return false;
+                }
+
+                var metadata = metaProp.GetValue(descriptor, null);
+                var tPropMeta = Type.GetType("System.Windows.PropertyMetadata, WindowsBase, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+
+                if (metadata == null
+                    || tPropMeta == null)
+                {
+                    return false;
+                }
+
+                var dvProp = tPropMeta.GetTypeInfo().GetDeclaredProperty("DefaultValue");
+
+                if (dvProp == null)
+                {
+                    return false;
+                }
+
+                var dv = (bool)dvProp.GetValue(metadata, null);
+                return dv;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+#endif
     }
 
     internal enum DesignerPlatformLibrary
