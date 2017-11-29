37fd589b9b41474dc344fc462a9e803bbda0ba6a
Lbugnion <lbugnion@outlook.com>
2017-11-29 15:10:25 +0100

    Added SimpleIocTestExceptions to check and close some issues

---
+++ GalaSoft.MvvmLight/Tests/PCL/GalaSoft.MvvmLight.Test (PNET45)/Ioc/SimpleIocTestExceptions.cs
@@ -0,0 +1,114 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GalaSoft.MvvmLight.Test.Ioc
+{
+    [TestClass]
+    public class SimpleIocTestExceptions
+    {
+        [TestMethod]
+        public void TestViewModelExceptionsInConstructor()
+        {
+            Exception error = null;
+            TestViewModelForExceptions vm = null;
+
+            if (!SimpleIoc.Default.IsRegistered<TestViewModelForExceptions>())
+            {
+                SimpleIoc.Default.Register<TestViewModelForExceptions>();
+            }
+
+            try
+            {
+                vm = SimpleIoc.Default.GetInstance<TestViewModelForExceptions>();
+            }
+            catch (Exception ex)
+            {
+                error = ex;
+            }
+
+            Assert.IsNotNull(error);
+        }
+
+        [TestMethod]
+        public void TestViewModelWithMultipleConstructors()
+        {
+            TestViewModelForMultiConstructors vm = null;
+
+            if (!SimpleIoc.Default.IsRegistered<TestViewModelForMultiConstructors>())
+            {
+                SimpleIoc.Default.Register<TestViewModelForMultiConstructors>();
+            }
+
+            if (!SimpleIoc.Default.IsRegistered<IService>())
+            {
+                SimpleIoc.Default.Register<IService, Service>();
+            }
+
+            vm = SimpleIoc.Default.GetInstance<TestViewModelForMultiConstructors>();
+        }
+
+        [TestMethod]
+        public void TestViewModelWithStaticConstructor()
+        {
+            TestViewModelForStaticConstructor vm = null;
+
+            if (!SimpleIoc.Default.IsRegistered<TestViewModelForStaticConstructor>())
+            {
+                SimpleIoc.Default.Register<TestViewModelForStaticConstructor>();
+            }
+
+            if (!SimpleIoc.Default.IsRegistered<IService>())
+            {
+                SimpleIoc.Default.Register<IService, Service>();
+            }
+
+            vm = SimpleIoc.Default.GetInstance<TestViewModelForStaticConstructor>();
+        }
+
+        public class TestViewModelForExceptions
+        {
+            public TestViewModelForExceptions()
+            {
+                throw new Exception("This is a test exception");
+            }
+        }
+
+        public class TestViewModelForMultiConstructors
+        {
+            private IService _service;
+
+            public TestViewModelForMultiConstructors()
+            {
+            }
+
+            [PreferredConstructor]
+            public TestViewModelForMultiConstructors(IService service)
+            {
+                _service = service;
+            }
+        }
+
+        public class TestViewModelForStaticConstructor
+        {
+            private IService _service;
+
+            static TestViewModelForStaticConstructor()
+            {
+            }
+
+            public TestViewModelForStaticConstructor(IService service)
+            {
+                _service = service;
+            }
+        }
+
+        public interface IService
+        {
+        }
+
+        public class Service : IService
+        {
+        }
+    }
+}
