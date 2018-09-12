4eb33fecd95dcb3805e3c810015354e587a00967
Laurent Bugnion <lbugnion@outlook.com>
2018-09-12 10:24:26 +0200

    Fixing MvvmLightStd10.V5.nuspec to use a specific ViewModelLocator without CommonServiceLocator

---
+++ NuGet/V5Full/Content/ViewModel/ViewModelLocatorStd10.cs.pp
@@ -0,0 +1,57 @@
+﻿/*
+  For Windows only:
+  
+  In App.xaml:
+  <Application.Resources>
+      <vm:ViewModelLocator xmlns:vm="clr-namespace:$rootnamespace$"
+                           x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+
+  You can also use Blend to do all this with the tool's support.
+  
+  More details:
+  See http://www.mvvmlight.net/installing/nuget
+*/
+
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace $rootnamespace$.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main
+        {
+            get
+            {
+                return SimpleIoc.Default.GetInstance<MainViewModel>();
+            }
+        }
+    }
+}
