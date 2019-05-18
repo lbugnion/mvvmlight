a20854257f4c9eb81335aed0400abab4faf4fd48
Calvé, Patrice <patware@hotmail.com>
2019-05-18 10:14:33 -0400

    Made changes to the StandardVSIX VS2019 files to match the VS2019 values

--- Installer/StandardVSIX/MvvmLight.VS2019/Properties/AssemblyInfo.cs
+++ Installer/StandardVSIX/MvvmLight.VS2019/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvvmLight.VS2017")]
+[assembly: AssemblyTitle("MvvmLight.VS2019")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MvvmLight.VS2017")]
+[assembly: AssemblyProduct("MvvmLight.VS2019")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
